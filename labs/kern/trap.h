2203a4142f63eb5dc02becb2a9f7b01befa27be2
Nikolay Amiantov <ab@fmap.me>
2015-12-05 17:52:32 +0300

    implement trap handlers, including _alltraps

--- labs/kern/trap.h
+++ labs/kern/trap.h
@@ -18,4 +18,28 @@ void print_trapframe(struct Trapframe *tf);
 void page_fault_handler(struct Trapframe *);
 void backtrace(struct Trapframe *);
 
+/* Interrupt handlers */
+void inthandle_divide();
+void inthandle_debug();
+void inthandle_nmi();
+void inthandle_brkpt();
+void inthandle_oflow();
+void inthandle_bound();
+void inthandle_illop();
+void inthandle_device();
+void inthandle_dblflt();
+
+void inthandle_tss();
+void inthandle_segnp();
+void inthandle_stack();
+void inthandle_gpflt();
+void inthandle_pgflt();
+
+void inthandle_fperr();
+void inthandle_align();
+void inthandle_mchk();
+void inthandle_simderr();
+
+void inthandle_syscall();
+
 #endif /* JOS_KERN_TRAP_H */
